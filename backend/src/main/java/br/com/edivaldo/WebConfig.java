2c7841dac5841eadcf8a28d51e5698bb79b1915e
Edivaldo <edi@example.com>
2024-01-10 14:11:28 -0300

    atualizar swagger

--- backend/src/main/java/br/com/edivaldo/WebConfig.java
+++ backend/src/main/java/br/com/edivaldo/WebConfig.java
@@ -12,7 +12,23 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import java.util.Collections;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.DocExpansion;
+import springfox.documentation.swagger.web.ModelRendering;
+import springfox.documentation.swagger.web.OperationsSorter;
+import springfox.documentation.swagger.web.TagsSorter;
+import springfox.documentation.swagger.web.UiConfiguration;
+import springfox.documentation.swagger.web.UiConfigurationBuilder;
 @Configuration
 @EnableSwagger2
 public class WebConfig {
@@ -26,17 +42,56 @@ public class WebConfig {
             }
         };
     }
-    @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2).select()
-                .apis(RequestHandlerSelectors.basePackage("br.com.edivaldo.controller"))
-                .paths(PathSelectors.any()).build()
-                .apiInfo(apiInfo());
-    }
+//    @Bean
+//    public Docket api() {
+//        return new Docket(DocumentationType.SWAGGER_2).select()
+//                .apis(RequestHandlerSelectors.basePackage("br.com.edivaldo.controller"))
+//                .paths(PathSelectors.any()).build()
+//                .apiInfo(apiInfo());
+//    }
+//
+//    private ApiInfo apiInfo() {
+//        return new ApiInfoBuilder().title("PORTFOLIO DE PROJETOS API")
+//                .description("Documentação da API de acesso aos endpoints da PORTFOLIO DE PROJETOS").version("1.0")
+//                .build();
+//    }
 
     private ApiInfo apiInfo() {
-        return new ApiInfoBuilder().title("PORTFOLIO DE PROJETOS API")
-                .description("Documentação da API de acesso aos endpoints da PORTFOLIO DE PROJETOS").version("1.0")
+        return new ApiInfo("My REST API", "Some custom description of API.", "API TOS", "Terms of service",
+                new Contact("Umang Budhwar", "www.baeldung.com", "umangbudhwar@gmail.com"),
+                "License of API", "API license URL", Collections.emptyList());
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    /**
+     * SwaggerUI information
+     */
+
+    @Bean
+    UiConfiguration uiConfig() {
+        return UiConfigurationBuilder.builder()
+                .deepLinking(true)
+                .displayOperationId(false)
+                .defaultModelsExpandDepth(1)
+                .defaultModelExpandDepth(1)
+                .defaultModelRendering(ModelRendering.EXAMPLE)
+                .displayRequestDuration(false)
+                .docExpansion(DocExpansion.NONE)
+                .filter(false)
+                .maxDisplayedTags(null)
+                .operationsSorter(OperationsSorter.ALPHA)
+                .showExtensions(false)
+                .tagsSorter(TagsSorter.ALPHA)
+                .supportedSubmitMethods(UiConfiguration.Constants.DEFAULT_SUBMIT_METHODS)
+                .validatorUrl(null)
                 .build();
     }
 }
